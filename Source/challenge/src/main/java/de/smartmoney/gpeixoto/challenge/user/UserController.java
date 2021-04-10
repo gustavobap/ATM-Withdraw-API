41c65c5d8f69c741e68f6f24f37b382703a3eedd
Gustavo Peixoto <gustavo.bul@gmail.com>
2021-04-10 23:56:13 +0000

    rename methods

--- Source/challenge/src/main/java/de/smartmoney/gpeixoto/challenge/user/UserController.java
+++ Source/challenge/src/main/java/de/smartmoney/gpeixoto/challenge/user/UserController.java
@@ -48,7 +48,7 @@ public class UserController extends ApplicationController {
     }
     
 	@GetMapping()
-	public ResponseEntity<List<User>> listAll() {
+	public ResponseEntity<List<User>> list() {
 		List<User> users = repository.findAll();
 		return ResponseEntity.ok().body(users);
 	}
